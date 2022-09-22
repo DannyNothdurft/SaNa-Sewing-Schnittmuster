a165aceef994951a6850a05a9b6a7ac3e6da094e
Danny Nothdurft <d.nothdurft@gmail.de>
2022-09-22 12:54:07 +0200

    reducer provisorisch angelegt

--- webapp/src/components/App.jsx
+++ webapp/src/components/App.jsx
@@ -11,9 +11,29 @@ import Artikel from "../pages/Artikel";
 import SingleArtikel from "../pages/SingleArtikel";
 import Footer from "./Footer";
 
+/*
+<div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+ */
 
 function App() {
   const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch()
 
   const [data, setData] = useState(dataMuster);
   const [suche, setSuche] = useState([]);
